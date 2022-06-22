f2bf4998000d45cceb63f6240d5205ffb85d845c
Henningson <jann-ole.henningson@fau.de>
2022-06-22 09:40:28 +0200

    Changed visualization script a bit, to allow for faster swapping of videos; added images for readme

--- visualize.py
+++ visualize.py
@@ -19,19 +19,21 @@ def draw_midlines(image, line):
 
 if __name__ == "__main__":
     # Open File
-    with open("dataset/CM/CM.json") as file:
+    name = "DD"
+    
+    with open("dataset/{0}/{0}.json".format(name)) as file:
         # Load JSON File
-        CM_DICT = json.load(file)
+        DICT = json.load(file)
 
-    cap = cv2.VideoCapture('dataset/CM/CM.avi')
+    cap = cv2.VideoCapture('dataset/{0}/{0}.avi'.format(name))
 
     #Printing available keys
-    print(CM_DICT.keys())
+    print(DICT.keys())
 
     # Extract glottal midlines, segmentations and laserdots
-    glottal_segmentations = np.array(CM_DICT["GlottalSegmentation"])
-    glottal_midlines = np.array(CM_DICT["GlottalMidline"])
-    laserdots_2d = np.array(CM_DICT["2DPoints"])
+    glottal_segmentations = np.array(DICT["GlottalSegmentation"])
+    glottal_midlines = np.array(DICT["GlottalMidline"])
+    laserdots_2d = np.array(DICT["2DPoints"])
 
     # Transforming from FRAME_NUM x X x Y x 2 to FRAME_NUM x X*Y x 2
     laserdots_2d = laserdots_2d.reshape(laserdots_2d.shape[0], -1, 2)
@@ -44,7 +46,7 @@ if __name__ == "__main__":
         draw_segmentation(frame, glottal_segmentations[frame_num])
         draw_midlines(frame, glottal_midlines[frame_num])
 
-        cv2.imshow("CM", frame)
+        cv2.imshow(name, frame)
         cv2.waitKey(0)
 
         frame_num += 1
